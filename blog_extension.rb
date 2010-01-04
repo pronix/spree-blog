5fd1451c787dbc51c0359101cc7c1e762cc76da0
Matt Mitchell <goodieboy@gmail.com>
2010-01-03 19:23:35 -0500

    added blog to admin tabs added US/UK translations for "blog"

--- blog_extension.rb
+++ blog_extension.rb
@@ -18,11 +18,13 @@ class BlogExtension < Spree::Extension
     # admin.tabs.add "Blog", "/admin/blog", :after => "Layouts", :visibility => [:all]
 
     # Add a link to the blog administration page on the configuration page
-    Admin::ConfigurationsController.class_eval do
-      before_filter :add_blog_links, :only => :index
-    
+    Admin::BaseController.class_eval do
+      before_filter :add_blog_links#, :only => :index
+      
       def add_blog_links
-        @extension_links << {:link => admin_blog_entries_path, :link_text => t('blog_management'), :description => t('blog_management_description')}
+        #@extension_links << {:link => admin_blog_entries_path, :link_text => t('blog_management'), :description => t('blog_management_description')}
+        
+        @extension_tabs << [:blog, {:route => :admin_blog_entries, :match_path => '/blog'}]#{:link => admin_blog_entries_path, :link_text => t('blog_management'), :description => t('blog_management_description')}
       end
     end
     
